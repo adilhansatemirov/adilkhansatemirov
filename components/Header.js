6f2aaf7283ef44985980a19735c646e4a3ee4e5e
adilhansatemirovv@gmail.com <adilhansatemirovv@gmail.com>
2020-05-10 04:47:32 +0600

    feat: change header

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,10 @@ export default () => (
   <div className="header">
     <div className="header__title-container">
       <h1 className="header__title">
-        <span className="header__title-text">Hi, I'm</span>
-        <span className="header__title-text">Adilkhan Satemirov</span>
+        <span className="header__title-greetings">Hi, I'm</span>
+        <span className="header__title-name">
+          <span>Adilkhan</span> <span>Satemirov</span>
+        </span>
       </h1>
       <h2 className="header__subtitle">Welcome to my page!</h2>
     </div>
